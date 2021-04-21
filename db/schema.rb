d04e7f91bcea6695f16a829f4d5562a7dc46302a
ianhutson <ih00209@georgiasouthern.edu>
2021-04-20 22:41:40 -0400

    backend looks good

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_221036) do
+ActiveRecord::Schema.define(version: 2021_04_21_023921) do
 
   create_table "auctions", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_04_09_221036) do
     t.string "twitch_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "hearts"
   end
 
 end
