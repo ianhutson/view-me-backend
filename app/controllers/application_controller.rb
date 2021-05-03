3929d0750b57b985e49e8e429af962c6c925043e
ianhutson <ih00209@georgiasouthern.edu>
2021-05-03 14:33:16 -0400

    jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,29 @@
 class ApplicationController < ActionController::API  
 
-  private
-
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+  def jwt_key
+    ENV[‘SESSION_SECRET’]
   end
-
+  def issue_token(user)
+    JWT.encode({user_id: user.id}, jwt_key, ‘HS256’)
+  end
+  def decoded_token
+    begin
+      JWT.decode(token, jwt_key, true, { :algorithm => ‘HS256’ })
+    rescue JWT::DecodeError
+      [{error: “Invalid Token”}]
+    end
+  end
+  def token
+    request.headers[‘Authorization’]
+  end
+  def user_id
+    decoded_token.first[‘user_id’]
+  end
+  def current_user
+    user ||= User.find_by(id: user_id)
+  end
   def logged_in?
     !!current_user
   end
-  
 
 end
